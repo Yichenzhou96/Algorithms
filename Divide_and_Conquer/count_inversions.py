852eb09a64d9f8eb7277b5828cefc5aff730c1fb
Yichen <yichenzhou96@outlook.com>
2020-10-21 23:20:46 +0100

    implement a fast divide-and-conquer algorithm to count inversions

---
+++ Divide_and_Conquer/count_inversions.py
@@ -0,0 +1,46 @@
+def sorted_inversions(array):
+    if len(array) <= 1:
+        return 0, array
+
+    n = len(array)//2
+    array_left = array[:n]
+    array_right = array[n:]
+
+    count_left, left_sorted = sorted_inversions(array_left)
+    count_right, right_sorted = sorted_inversions(array_right)
+
+    i = 0
+    j = 0
+    k = 0
+    n_new = len(left_sorted) + len(right_sorted)
+    new_array = [0 for _ in range(n_new)]
+    count = 0
+    while i < len(left_sorted) and j < len(right_sorted):
+        # print(i, j, k)
+        if left_sorted[i] < right_sorted[j]:
+            new_array[k] = left_sorted[i]
+            i += 1
+            k += 1
+        else:
+            new_array[k] = right_sorted[j]
+            count = count + len(left_sorted) - i
+            j += 1
+            k += 1
+
+    while i < len(left_sorted):
+        new_array[k] = left_sorted[i]
+        i += 1
+        k += 1
+
+    while j < len(right_sorted):
+        new_array[k] = right_sorted[j]
+        j += 1
+        k += 1
+
+    return count_right + count_left + count, new_array
+
+
+if __name__ == '__main__':
+
+    content = [1, 3, 5, 2, 4, 6, 9, 8, 10, 11, 16, 15]
+    print(sorted_inversions(content)[0])
